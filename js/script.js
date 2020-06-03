8a5d548c860666efe1ab4065691aa19bba835f72
Aldo Cahuana <aldooow@icloud.com>
2020-06-03 19:49:49 +0200

    Work in progress!!

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ BONUS: all’inizio il software richiede anche una difficoltà all’utente che 
 Con difficoltà 0=> tra 1 e 100, con difficoltà 1 => tra 1 e 80, con difficoltà 2=> tra 1 e 50
 Raccomandazione: andato avanti come sempre un pezzettino alla volta, diamo a variabili e funzioni nomi che riflettano il proprio ruolo e
 prima di iniziare facciamo l’analisi e scriviamo i punti importanti!*/
+
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+var listaNumeriRandom = [];
+numeriRandom(listaNumeriRandom);
+console.log(listaNumeriRandom)
+
+// In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
+var numeroUtente = parseFloat(prompt('Inserire un numero compreso tra 1 e 100!!!'))
+console.log('Questo è il mio numero: ' + numeroUtente)
+
+
+
+
+// FUNZIONI
+function numeriRandom(lista){
+  for(var i = 1; i <= 16; i++){
+    var num = Math.round(Math.random()*100+1);
+    lista.push(num);
+ }
+}
