0e9c84880d226ab08ae80190e441822e23d0ec6b
Aldo Cahuana <aldooow@icloud.com>
2020-06-05 17:03:10 +0200

    Game Completed!

--- js/script.js
+++ js/script.js
@@ -9,49 +9,57 @@ Raccomandazione: andato avanti come sempre un pezzettino alla volta, diamo a var
 prima di iniziare facciamo l’analisi e scriviamo i punti importanti!*/
 
 
-
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-var quantitaNumeriCasuali = 16;
-var rangeNumeriCasuali = 100;
-var arrayBombe = generaNumeriCasuali(quantitaNumeriCasuali, rangeNumeriCasuali);
-
-
-
-
-// In seguito deve chiedere all’utente di inserire un numero alla volta,
-// sempre compreso tra 1 e 100.
-
-
-
-// Se il numero è presente nella lista dei numeri generati, la partita termina,
-// altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato”
-// o raggiunge il numero massimo possibile di numeri consentiti.
-var numeroMassimoGiocate = rangeNumeriCasuali - quantitaNumeriCasuali;
-console.log(numeroMassimoGiocate);
-
-var numeroInsersioniUtente = 0;
-var bombaBeccata = false;
-
-while((bombaBeccata === false) && (numeroInsersioniUtente < numeroMassimoGiocate)){
-  var numeroUtente = parseInt(prompt('inserire un numero del 1 al ' + rangeNumeriCasuali))
-  if(verificaNumeroInArray(numeroUtente, arrayBombe) === true) {
-    bombaBeccata = true;
-  }
-  numeroInsersioniUtente++
-}
-console.log(numeroInsersioniUtente)
-
-// Al termine della partita il software deve comunicare il punteggio,
-// cioè il numero di volte che l’utente ha inserito un numero consentito.
-
-
 // BONUS: all’inizio il software richiede anche una difficoltà all’utente che
 // cambia il range di numeri casuali.
 // Con difficoltà 0=> tra 1 e 100,
 // con difficoltà 1 => tra 1 e 80,
 // con difficoltà 2=> tra 1 e 50.
 
+var difficolta = prompt('scegli il grado di difficolta fra 0,1,2!!!');
+var rangeNumeriCasuali;
+if(difficolta == '0'){
+  rangeNumeriCasuali = 100;
+} else if (difficolta == '1'){
+  rangeNumeriCasuali = 80;
+} else if (difficolta == '2'){
+  rangeNumeriCasuali = 50;
+}
+console.log('Livello di difficolta: ' + difficolta)
+
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+var quantitaNumeriCasuali = 16;
+// var rangeNumeriCasuali = 100;
+var arrayBombe = generaNumeriCasuali(quantitaNumeriCasuali, rangeNumeriCasuali);
+console.log('QUESTE SONO LE MIE BOMBE: ', arrayBombe)
+
+// In seguito deve chiedere all’utente di inserire un numero alla volta,
+// sempre compreso tra 1 e 100.
+
+// var numeroUtente = parseInt(prompt('inserire un numero del 1 al ' + rangeNumeriCasuali))
+
+// Se il numero è presente nella lista dei numeri generati, la partita termina,
+// altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato”
+// o raggiunge il numero massimo possibile di numeri consentiti.
+var numeroMassimoGiocate = rangeNumeriCasuali - quantitaNumeriCasuali;
+console.log('PUNTEGGIO MASSIMO: ' + numeroMassimoGiocate);
+
+var numeroInsersioniUtente = 0;
+var bombaBeccata = false;
+
+while(bombaBeccata === false && (numeroInsersioniUtente < numeroMassimoGiocate)){
+  var numeroUtente = parseInt(prompt('inserire un numero del 1 al ' + rangeNumeriCasuali))
+  if(verificaNumeroInArray(numeroUtente, arrayBombe) === true) {
+    bombaBeccata = true;
+    console.log('BOMBA!!!')
+  }
+  numeroInsersioniUtente++
+  console.log('Intento: ' + numeroInsersioniUtente)
+}
+
+// Al termine della partita il software deve comunicare il punteggio,
+// cioè il numero di volte che l’utente ha inserito un numero consentito.
+console.log('PUNTEGGIO UTENTE : ' + numeroInsersioniUtente)
 
 
 // FUNZIONI!!!
@@ -67,7 +75,7 @@ function generaNumeriCasuali(quantitaNumeriCasuali, rangeNumeriCasuali ) {
       arrayBombe.push(numeroCasuale);
     }
   }
-  console.log(arrayBombe);
+return arrayBombe;
 }
 
 // Questa fuznione verifica che non si ripeta un elemento dentro il Array!
